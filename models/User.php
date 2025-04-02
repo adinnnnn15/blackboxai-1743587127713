83730b62c6e72614b0d24b9a855cbc177c5590c2
adinnnnn15 <starchewing@gmail.com>
2025-04-02 21:06:07 +0800

    Add models/User.php 1743599165428

---
+++ models/User.php
@@ -0,0 +1,41 @@
+<?php
+class User {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    // Register new user
+    public function register($data) {
+        $this->db->query('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', password_hash($data['password'], PASSWORD_DEFAULT));
+
+        return $this->db->execute();
+    }
+
+    // Login user
+    public function login($email, $password) {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->single();
+
+        if ($row && password_verify($password, $row->password)) {
+            return $row;
+        }
+        return false;
+    }
+
+    // Find user by email
+    public function findUserByEmail($email) {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+        $this->db->single();
+
+        return $this->db->rowCount() > 0;
+    }
+}
+?>
